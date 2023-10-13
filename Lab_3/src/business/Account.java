8f48e8b53aa83fc230467d5359be7e840479f639
aswinlakshmanan-s <sriram.a@northeastern.edu>
2023-10-12 22:14:19 -0400

    Lab 3 - Bank Account

---
+++ Lab_3/src/business/Account.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aswinlakshmanan
+ */
+public class Account {
+      private String routingNumber;
+    private String accountNumber;
+    private String bankName;
+    private int balance;
+    private Date createdDate;
+
+    
+    
+    public Account() {
+        
+        this.createdDate=new Date();
+    }
+    
+        
+
+    public String getRoutingNumber() {
+        return routingNumber;
+    }
+
+    public void setRoutingNumber(String routingNumber) {
+        this.routingNumber = routingNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+    
+    public String toString(){
+        return bankName;
+    }
+    
+    
+}
